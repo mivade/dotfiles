476f36161fc8436128a042870abf832bd01a35db
Michael V. DePalatis <mike@depalatis.net>
2015-01-12 21:07:56 +0100

    Add simple setup script
    TODO:
    
    * Write a function to remove repetetive typing for making links
    * Do things like global git author/email config
    * Only warn on failure?

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Setup links and other basic common functionality for a new
+# environment.
+
+ln -s $HOME/dotfiles/emacs $HOME/.emacs
+# TODO: make this overwrite if it exists
+ln -s $HOME/dotfiles/emacs.d $HOME/.emacs.d
