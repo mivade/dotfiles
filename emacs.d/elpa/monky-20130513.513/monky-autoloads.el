a7a004c4ce46b607ba77846d5a304c69656bdae4
Michael V. DePalatis <mike@depalatis.net>
2015-02-08 11:57:03 +0100

    Add monky

---
+++ emacs.d/elpa/monky-20130513.513/monky-autoloads.el
@@ -0,0 +1,25 @@
+;;; monky-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "monky" "monky.el" (21719 16474 564676 513000))
+;;; Generated autoloads from monky.el
+
+(autoload 'monky-status "monky" "\
+Show the status of Hg repository.
+
+\(fn &optional DIRECTORY)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("monky-pkg.el") (21719 16474 800445 599000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; monky-autoloads.el ends here
