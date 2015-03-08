c0f603cad032be8c9c8a28be217edb8f5c5e6416
Michael V. DePalatis <mike@depalatis.net>
2015-03-08 06:43:41 +0100

    Default to Python 3

---
+++ bash_aliases
@@ -0,0 +1,5 @@
+# -*- bash -*-
+alias python=python3
+alias pip=pip3
+alias virtualenv=virtualenv3
+alias ipython=ipython3
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # Setup links and other basic common functionality for a new
 # environment.
 
+ln -s $HOME/dotfiles/bash_aliases $HOME/.bash_aliases
 ln -s $HOME/dotfiles/emacs $HOME/.emacs
 # TODO: make this overwrite if it exists
 ln -s $HOME/dotfiles/emacs.d $HOME/.emacs.d
