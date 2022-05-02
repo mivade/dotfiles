e824fbe1cd485603e30d58ddb1b608096fb1fdf4
Michael V. DePalatis <mike@depalatis.net>
2022-05-02 13:31:01 -0600

    Install themes in themes.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,18 +22,14 @@
 (require 'use-package)
 (setq use-package-always-ensure 't)
 
-;; Color theme
-;(use-package zenburn-theme)
-;(load-theme 'zenburn t)
-;(use-package monokai-theme)
-;(load-theme 'monokai t)
-(use-package firecode-theme)
-(load-theme 'firecode t)
-
 ;; Load additional functionality from other files
 (load "~/.emacs.d/ide.el")
 (load "~/.emacs.d/git.el")
 (load "~/.emacs.d/markdown.el")
 (load "~/.emacs.d/python.el")
 (load "~/.emacs.d/terraform.el")
+(load "~/.emacs.d/themes.el")
 (load "~/.emacs.d/web.el")
+
+;; Set color theme
+(load-theme 'firecode t)
---
+++ emacs/.emacs.d/themes.el
@@ -0,0 +1,4 @@
+;; Install third-party themes
+(use-package firecode-theme)
+(use-package monokai-theme)
+(use-package zenburn-theme)
