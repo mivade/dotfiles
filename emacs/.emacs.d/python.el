810438b79a7b34c9717e52dc52ad4f3c53b69e82
Michael V. DePalatis <mike@depalatis.net>
2022-05-03 13:39:13 -0600

    Update Python configuration for emacs

--- emacs/.emacs.d/python.el
+++ emacs/.emacs.d/python.el
@@ -1,8 +1,12 @@
 ;; Python
-;; FIXME: getting errors here (error in process sentinel)
 ;; See https://realpython.com/emacs-the-best-python-editor/
+;; Note that if getting strange errors about a process exiting with code 1
+;; it's likely because `virtualenv` cannot be found (see output of
+;; `M-x elpy-config` to confirm). Just make sure that `virtualenv` is
+;; installed (e.g., `conda install -y virtualenv`)
 (use-package elpy
   :defer t
+  :commands elpy-enable
+  :init
+  (with-eval-after-load 'python (elpy-enable))
 )
-;; FIXME: figure out how to run automatically when loading elpy
-(elpy-enable)
