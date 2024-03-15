de13325eec38cb2277d40415f4d800edbdf36951
Michael V. DePalatis <mdepalatis@ascendanalytics.com>
2024-03-15 12:21:39 -0600

    Enable org-babel languages

--- emacs/.emacs.d/org.el
+++ emacs/.emacs.d/org.el
@@ -3,3 +3,15 @@
 ;; Structure templates
 ;; https://orgmode.org/manual/Structure-Templates.html
 (require 'org-tempo)
+
+;; Evaluating code blocks
+;; https://orgmode.org/worg/org-contrib/babel/languages/index.html#configure
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+   (C . t)
+   (python . t)
+   (shell . t)
+   (sqlite . t)
+  )
+)
